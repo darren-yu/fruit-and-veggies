715bd981a6e886d0189253f1a3ba55e989e0d5c2
darren-yu <darren.d.yu@gmail.com>
2015-02-03 12:58:04 -0800

    need to finish win condition

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
-/* setup your angular app here */
+var fruitveggie = angular.module("FruitVeggie", []);
 
-//debug stuff to show the app is loading and fruit / veggies are available
-console.log('App Started');
-console.log('Fruit count', fruit.length);
-console.log('Veggie count', vegetables.length);
+
+fruitveggie.run(function() {
+    console.log('App Started');
+    console.log('Fruit count', fruit.length);
+    console.log('Veggie count', vegetables.length);
+});
+
+fruitveggie.controller("List",["$scope",function($scope){
+
+    var combineList = fruit.concat(vegetables);
+    $scope.items = combineList;
+    $scope.fruits = [];
+    $scope.veggies = [];
+
+    $scope.moveLeft = function(idx) {
+        $scope.fruits.push($scope.items[idx]);
+        $scope.items.splice(idx,1);
+    }
+
+    $scope.moveRight = function(idx) {
+        $scope.veggies.push($scope.items[idx]);
+        $scope.items.splice(idx,1);
+    }
+
+    $scope.moveBackRight = function(idx) {
+        $scope.items.push($scope.fruits[idx]);
+        $scope.fruits.splice(idx,1);
+    }
+
+    $scope.moveBackLeft = function(idx) {
+        $scope.items.push($scope.veggies[idx]);
+        $scope.veggies.splice(idx,1);
+    }
+
+
+
+
+
+}])
+
+
